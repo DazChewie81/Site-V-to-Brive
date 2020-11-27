b611041fc7eb6b9f0e6e268e81d5f14a33d64f16
DazChewie <62639989+DazChewie81@users.noreply.github.com>
2020-11-27 20:36:42 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+		<title>Les Vétérinaires de Brive-La-Gaillarde</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta charset="utf-8">
+    	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-md bg-info navbar-dark">
+			<a class="navbar-brand" href="index.html">Les Vétérinaires de Brive</a>
+			<ul class="navbar-nav">
+      			<li class="nav-item">
+       				<a class="nav-link" href="index.html">Accueil</a>
+      			</li>
+      			<li class="nav-item ">
+         			<a class="nav-link" href="#">Horaires</a>
+      			</li>
+
+   			</ul>
+   		</nav>
+	</body>
+</html>
